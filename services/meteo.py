7e7eebfd56add09b54ef94ca280304bc9cc5c207
francescoDefulgentiis <fran.cesco.defulgentiis@gmail.com>
2025-01-05 18:00:14 +0100

    now meteo returns the correct data.

--- services/meteo.py
+++ services/meteo.py
@@ -4,42 +4,64 @@ import os
 from time import sleep
 from dotenv import load_dotenv
 class Meteo_controller:
-    def __init__(self, locationId, unit):
+    def __init__(self, location, unit):
         load_dotenv()
-        self.locationId = locationId
+        self.location = location
         self.unit = unit
         self.stop_flag= False
         self.response = None
         self.thread = None
-        self.appid = os.getenv('OPENWEATHER_API_KEY')
-        self.base_url = 'http://api.openweathermap.org/data/2.5//forecast'
+        self.key = os.getenv('OPENWEATHER_API_KEY')
+        self.forecast_url = 'http://api.weatherapi.com/v1/forecast.json'
 
     def setStopFlag(self, status):
         self.stop_flag = status
         
     def thread_function(self):
 
-        params = {
-            'id': self.locationId,
-            'units': self.unit,
-            'appid': self.appid,
-            'lang': 'en',
-            'mode': 'json'
+        forecast_params = {
+            'q': self.location,
+            'key': self.key,
+            'aqi': 'yes',
         }
         
         while not self.stop_flag:
             print("Fetching data from the API")
-            url = requests.Request('GET', self.base_url, params=params).prepare().url
-            response = requests.get(url)
-            if response.status_code == 200:
-                data = response.json()
-                #analisi e scrematura dati
+            forecast_url = requests.Request('GET', self.forecast_url, params=forecast_params).prepare().url
+
+            forecast_response = requests.get(forecast_url)
+
+            if forecast_response.status_code == 200:
+                weather_data = forecast_response.json()
+
+                forecasts = {}
+                for forecast in weather_data['forecast']['forecastday'][0]['hour']:
+                    forecasts[forecast['time']] = {
+                        'temp': forecast['temp_c'],
+                        'condition': forecast['condition']['text'],
+                        'precip': forecast['precip_mm']
+                }
+
+                data = {
+                    'location': self.location,
+                    'unit': self.unit,
+                    'max_temp': weather_data['forecast']['forecastday'][0]['day']['maxtemp_c'],
+                    'min_temp': weather_data['forecast']['forecastday'][0]['day']['mintemp_c'],
+                    'max_wind': weather_data['forecast']['forecastday'][0]['day']['maxwind_kph'],
+                    'total_precip': weather_data['forecast']['forecastday'][0]['day']['totalprecip_mm'],
+                    'avg_humidity': weather_data['forecast']['forecastday'][0]['day']['avghumidity'],
+                    'sunset': weather_data['forecast']['forecastday'][0]['astro']['sunset'],
+                    'sunrise': weather_data['forecast']['forecastday'][0]['astro']['sunrise'],
+                    'forecasts': forecasts
+                }
+
                 self.response = data
             else:
                 print("Error while fetching data from the API")
-                print("Status code: ", response.status_code)
+                print("status code: ", forecast_response.status_code)
 
             sleep(1)
+
         
     def start_thread(self,):
         self.stop_flag = False
